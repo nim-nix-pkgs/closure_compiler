d4a9f2e09c049f7a96aef82d88969b019fb82b57
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:24 -0300

    chore: re-index ../nimpkgs/c/closure_compiler/v0_3_1

--- v0_3_1/flake.nix
+++ v0_3_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Bindings for Closure Compiler web API.'';
+  description = ''Bindings to closure compiler web API'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,8 +9,8 @@
   
   inputs.src-closure_compiler-v0_3_1.flake = false;
   inputs.src-closure_compiler-v0_3_1.owner = "yglukhov";
-  inputs.src-closure_compiler-v0_3_1.ref   = "refs/tags/v0.3.1";
-  inputs.src-closure_compiler-v0_3_1.repo  = "closure_compiler";
+  inputs.src-closure_compiler-v0_3_1.ref   = "v0_3_1";
+  inputs.src-closure_compiler-v0_3_1.repo  = "closure_compiler.git";
   inputs.src-closure_compiler-v0_3_1.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
